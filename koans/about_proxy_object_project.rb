f1ff55385daca011ca091242ecbe52641d37590a
Jeduan Cornejo <jeduan@gmail.com>
2011-02-21 01:01:05 -0600

    Objeto proxy y pasado de mensajes

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,9 +13,39 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+  
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
+  end
+  
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+  
+  def number_of_times_called(method_name)
+    a = @messages.select do |x|
+      x == method_name
+    end
+    a.size
+  end
+  
+  def method_missing(method_name, *args, &block)
+    if @object.send(:respond_to?, method_name)
+      @messages << method_name
+      
+      if args.empty?
+        @object.send(method_name)
+      elsif args.size == 1
+        @object.send(method_name, args.first)
+      else
+        @object.send(method_name, args)
+      end
+    else
+      super.method_missing(method_name, args, block)
+    end
+    
   end
 
   # WRITE CODE HERE
